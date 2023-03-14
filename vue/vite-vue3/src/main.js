5b2d6b4f0917b3455fb39e0fefb38524a88149cc
Ducanoid Young <young0717@yahoo.com>
2023-03-14 22:26:14 +0800

    vue3 runnable

--- vue/vite-vue3/src/main.js
+++ vue/vite-vue3/src/main.js
@@ -1,5 +1,15 @@
 import { createApp } from 'vue'
-import './style.css'
+import './style/style.scss'
 import App from './App.vue'
+import router from "./router/router";
+import 'vant/lib/index.css';
+import { Form, Field, CellGroup, Button } from 'vant';
 
-createApp(App).mount('#app')
+createApp(App) 
+    .use(router)
+    // 引入vant组件，use方法注入后才能使用
+    .use(Form)
+    .use(Field)
+    .use(CellGroup)
+    .use(Button)
+    .mount('#app')
