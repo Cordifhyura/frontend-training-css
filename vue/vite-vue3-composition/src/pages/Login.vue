e46e258d1e1323f0587f2be98d4d42f5820a7c63
Ducanoid Young <young0717@yahoo.com>
2023-03-20 22:58:34 +0800

    react helloworld

--- vue/vite-vue3-composition/src/pages/Login.vue
+++ vue/vite-vue3-composition/src/pages/Login.vue
@@ -16,16 +16,43 @@
             placeholder="Password"
             :rules="[{ required: true, message: 'Password is required' }]"
             />
+            <van-field
+            v-model="rePassword"
+            v-show="!isLogin"
+            type="password"
+            name="rePassword"
+            label="Confirm password"
+            placeholder="Re-enter Password"
+            :rules="[{ required: true, message: 'Password is required' }]"
+            />
         </van-cell-group>
         <div style="margin: 16px;">
             <van-button round block class="grey" type="primary" native-type="submit">
             Submit
             </van-button>
         </div>
+        <van-col span="">
+            <span>forgot your password</span>
+        </van-col>
+        <van-row>
+            <van-col v-show="isLogin" span="12">
+                <a href="" @click.prevent @click="isLogin = false">
+                    need an account
+                </a>
+            </van-col>
+            <van-col v-show="!isLogin" span="12">
+                <a href="" @click.prevent @click="isLogin = true" >
+                    already have an account
+                </a>
+            </van-col>
+        </van-row>
     </van-form>
 </template>
 
 <script setup>
+    import { ref } from 'vue';
+
+    const isLogin = ref(true);
 
 </script>
 
@@ -37,6 +64,9 @@
 
 <!-- 在页面中覆盖第三方框架的样式，用scoped标签是无效的，要另起一个全局的style? -->
 <style scoped>
+    a {
+        margin: auto 0.25rem;
+    }
     .grey{
         background-color: #ccc;
     }
